47ef01c43e04e10cccfde0e840c9a2d24addd148
Gregory Bell <trihard.studios@outlook.com>
2025-06-27 15:36:49 -0600

    update migrations

--- persist/queries/migrations/01.sql
+++ persist/queries/migrations/01.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE TABLE kv_props (
+CREATE TABLE IF NOT EXISTS kv_props(
   key TEXT PRIMARY KEY,
   value TEXT NOT NULL
 );
@@ -10,19 +10,19 @@ INSERT INTO kv_props VALUES
 
 INSERT INTO kv_props VALUES ('user_version', '1');
 
-CREATE TABLE attributes (
+CREATE TABLE IF NOT EXISTS  attributes (
   id TEXT NOT NULL,
   attr TEXT NOT NULL
 );
 
 CREATE UNIQUE INDEX attributes_id_attr_idx ON attributes (id, attr);
 
-CREATE TABLE rate_limit_event (
+CREATE TABLE IF NOT EXISTS rate_limit_event (
   id TEXT PRIMARY KEY,
   timestamp TEXT NOT NULL
 );
 
-CREATE TABLE gigglesnort (
+CREATE TABLE IF NOT EXISTS gigglesnort (
   word TEXT PRIMARY KEY,
   message TEXT NOT NULL
 );
